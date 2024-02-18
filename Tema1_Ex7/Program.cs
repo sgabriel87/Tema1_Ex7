a4b864389afecebe2e286f19b39f51b993dfa006
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:03:39 +0200

    Add project files.

---
+++ Tema1_Ex7/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Ex 7
+Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+daca x este divizibil cu y.
+ In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+“indivizibil”.*/
+
+Console.WriteLine("Introduceti primul numar (x):");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti al doilea numar (y):");
+int y = int.Parse(Console.ReadLine());
+
+if (x % y == 0)
+{
+    Console.WriteLine("divizibil");
+}
+else
+{
+    Console.WriteLine("indivizibil");
+}
